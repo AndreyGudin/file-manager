a12ef28455335b146d1507bae79cbd5c9355e872
Andrey Gudin <aeon150791@gmail.com>
2022-12-10 18:18:04 +0400

    feat: add folder up and cd

--- file-manager.js
+++ file-manager.js
@@ -1,34 +1,62 @@
 import { argv } from "node:process";
 import { stdin, exit } from "process";
 import { homedir } from "node:os";
+import { join } from "path";
 import url from "url";
 
-const printCurrentWorkingDirectory = (path) => console.log(`You are currently in ${path}`);
+const printCurrentWorkingDirectory = (path) =>
+  console.log(`You are currently in ${path}`);
 
-const fileManager = () =>{
+const folderUp = (dir) => {
+  const newPath = url.fileURLToPath(new URL("..", `file://${dir}`));
+  printCurrentWorkingDirectory(newPath);
+  return newPath;
+};
+
+const changeDirectory = (path, currentPath) => {
+  const isAbsolutePath = path.split(":").length > 1 ? true : false;
+  const pathChanged = join(currentPath, path);
+  const newPath = isAbsolutePath ? url.fileURLToPath(new URL(".", `file://${path}`)) : url.fileURLToPath(new URL(".", `file://${pathChanged}`));
+  printCurrentWorkingDirectory(newPath);
+  return newPath;
+};
+
+const fileManager = () => {
   const username = argv[2].split("=")[1];
   const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
   const __homepage = url.fileURLToPath(new URL(".", `file://${homedir()}`));
   console.log(`Welcome to the File Manager ${username}!`);
   printCurrentWorkingDirectory(__homepage);
-  stdin.on("data",(data) => {
-   
-    
-    const command = data.toString().trim();
+  console.log("Enter command");
+  stdin.on("data", (data) => {
+    const command = data.toString().trim().split(" ")[0];
+    const param = data.toString().trim().split(" ")[1];
+    let currentPath = __homepage;
     console.log("Enter command");
 
+    switch (command) {
+      case ".exit":
+        console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+        exit();
+      case "up":
+        let path = currentPath;
+        currentPath = folderUp(path);
+        break;
+      case "cd":
+        let pathCD = currentPath;
+        if (param) currentPath = changeDirectory(param, pathCD);
+        else console.log("Invalid input");
+        break;
+      default:
+        console.log("Invalid input");
+        break;
+    }
 
-    if (command === ".exit") {
+    process.on("SIGINT", () => {
       console.log(`Thank you for using File Manager, ${username}, goodbye!`);
       exit();
-    }
-    process.on("SIGINT", () =>{
-      console.log(`Thank you for using File Manager, ${username}, goodbye!`);
-    })
-
-
-    
-  })
-}
+    });
+  });
+};
 
 fileManager();
